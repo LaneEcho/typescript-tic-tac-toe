1f1901a6e3ff803b6205f63068e35219a3b63325
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-08-24 15:46:31 -0700

    types.ts

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type Scoreboard = {
+  X: number;
+  O: number;
+};
+
+export type Player = 'X' | 'O';
+
+export type BoardText = '-' | 'X' | 'O';
+
+export type BoardContent = Array<BoardText>[];
+
+export type ServerError = {
+  log: string;
+  status?: number;
+  message: { err: string };
+};
