e782795fc3a35eebd94ebec4ceeec0ae1ee38871
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-08-25 15:45:18 -0700

    app

---
+++ src/client/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Board from './Components/Board';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  );
+};
+
+render(<App />, document.querySelector('#root'));
