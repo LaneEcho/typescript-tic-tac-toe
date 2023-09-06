cbb2188b5866f98c1c39b695c52e90a29d7ffd3f
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-09-06 12:07:52 -0700

    comment cleanup

--- src/client/Components/Board.tsx
+++ src/client/Components/Board.tsx
@@ -85,6 +85,73 @@ function Board() {
     } else alert("Choose a different box, this one's taken!");
   };
 
+  /**
+   * @method checkForWinner
+   * @description - checks to see if either player has filled a row
+   *  if so, ends the game and updates the message to declare winner
+   */
+
+  //   checkForWinner(): void {
+  //     const { board, gameOver, currentPlayer } = this.state;
+
+  //     // helper function to check if board is filled
+  //     const spacesLeft = (): boolean => {
+  //       for (let i of board) {
+  //         if (i.includes('-')) return true;
+  //       }
+  //       return false;
+  //     };
+
+  //     if (!gameOver) {
+  //       // win conditions: matching rows, columns, or diagonals, that are not empty('-')
+  //       if (
+  //         (board[0][0] === board[0][1] &&
+  //           board[0][1] === board[0][2] &&
+  //           board[0][2] !== '-') ||
+  //         (board[1][0] === board[1][1] &&
+  //           board[1][1] === board[1][2] &&
+  //           board[1][2] !== '-') ||
+  //         (board[2][0] === board[2][1] &&
+  //           board[2][1] === board[2][2] &&
+  //           board[2][2] !== '-') ||
+  //         (board[0][0] === board[1][0] &&
+  //           board[1][0] === board[2][0] &&
+  //           board[2][0] !== '-') ||
+  //         (board[0][1] === board[1][1] &&
+  //           board[1][1] === board[2][1] &&
+  //           board[2][1] !== '-') ||
+  //         (board[0][2] === board[1][2] &&
+  //           board[1][2] === board[2][2] &&
+  //           board[2][2] !== '-') ||
+  //         (board[0][0] === board[1][1] &&
+  //           board[1][1] === board[2][2] &&
+  //           board[2][2] !== '-') ||
+  //         (board[2][0] === board[1][1] &&
+  //           board[1][1] === board[0][2] &&
+  //           board[0][2] !== '-')
+  //       ) {
+  //         // winner is the person who's turn was previous
+  //         const winner: Player = currentPlayer === 'X' ? 'O' : 'X';
+
+  //         this.setState({
+  //           gameOver: true,
+  //           message: `Player ${winner} wins!`,
+  //         });
+
+  //         this.getScores('POST', JSON.stringify({ winner }));
+
+  //         // draw condition: no '-' remaining in board without above win condition triggering
+  //       } else if (!spacesLeft()) {
+  //         this.setState({
+  //           gameOver: true,
+  //           message: 'Draw!',
+  //         });
+  //       }
+  //     }
+  //   }
+
+  //   getScores(method?: string, winner?: string) {}
+
   // iterating to make Rows
   const rows: JSX.Element[] = [];
   for (let i = 0; i < 3; i++) {
@@ -113,128 +180,5 @@ function Board() {
     </div>
   );
 }
-
-// generic typing ensures whatever is typed as input is typed as output
-// class Board2 extends Component<{}, BoardState> {
-//   constructor(props: any) {
-//     super(props);
-//     this.state = {
-//       board: this.newBoard(),
-//       currentPlayer: 'X',
-//       gameOver: false,
-//       message: '',
-//       scoreboard: { X: 0, O: 0 },
-//     };
-
-//     // these methods need to be bound to the context of `this` since
-//     // these will be passed into event handlers and called from outside the object
-//     // where the context of `this` would be otherwise different
-//     this.resetBoard = this.resetBoard.bind(this);
-//     this.handleBoxClick = this.handleBoxClick.bind(this);
-//   }
-
-//   componentDidMount() {
-//     this.getScores();
-//   }
-
-//   componentDidUpdate() {
-//     this.checkForWinner();
-//   }
-
-//   /**
-//    * @method checkForWinner
-//    * @description - checks to see if either player has filled a row
-//    *  if so, ends the game and updates the message to declare winner
-//    */
-//   checkForWinner(): void {
-//     const { board, gameOver, currentPlayer } = this.state;
-
-//     // helper function to check if board is filled
-//     const spacesLeft = (): boolean => {
-//       for (let i of board) {
-//         if (i.includes('-')) return true;
-//       }
-//       return false;
-//     };
-
-//     if (!gameOver) {
-//       // win conditions: matching rows, columns, or diagonals, that are not empty('-')
-//       if (
-//         (board[0][0] === board[0][1] &&
-//           board[0][1] === board[0][2] &&
-//           board[0][2] !== '-') ||
-//         (board[1][0] === board[1][1] &&
-//           board[1][1] === board[1][2] &&
-//           board[1][2] !== '-') ||
-//         (board[2][0] === board[2][1] &&
-//           board[2][1] === board[2][2] &&
-//           board[2][2] !== '-') ||
-//         (board[0][0] === board[1][0] &&
-//           board[1][0] === board[2][0] &&
-//           board[2][0] !== '-') ||
-//         (board[0][1] === board[1][1] &&
-//           board[1][1] === board[2][1] &&
-//           board[2][1] !== '-') ||
-//         (board[0][2] === board[1][2] &&
-//           board[1][2] === board[2][2] &&
-//           board[2][2] !== '-') ||
-//         (board[0][0] === board[1][1] &&
-//           board[1][1] === board[2][2] &&
-//           board[2][2] !== '-') ||
-//         (board[2][0] === board[1][1] &&
-//           board[1][1] === board[0][2] &&
-//           board[0][2] !== '-')
-//       ) {
-//         // winner is the person who's turn was previous
-//         const winner: Player = currentPlayer === 'X' ? 'O' : 'X';
-
-//         this.setState({
-//           gameOver: true,
-//           message: `Player ${winner} wins!`,
-//         });
-
-//         this.getScores('POST', JSON.stringify({ winner }));
-
-//         // draw condition: no '-' remaining in board without above win condition triggering
-//       } else if (!spacesLeft()) {
-//         this.setState({
-//           gameOver: true,
-//           message: 'Draw!',
-//         });
-//       }
-//     }
-//   }
-
-//   getScores(method?: string, winner?: string) {}
-
-//   handleBoxClick() {}
-
-//   render() {
-// // insert logic to render rows here
-// let rows = [];
-// for (let i = 0; i > 3; i++) {
-//   rows.push(
-//     <Row row={i} content={[]} handleBoxClick={this.handleBoxClick} />
-//   );
-// }
-
-// // Destructure scores for X and O from state so that they can individually be rendered below
-// const { X, O }: Scoreboard = this.state.scoreboard;
-
-// return (
-//   <div className="board">
-//     {rows}
-//     <button id="reset" onClick={this.resetBoard}>
-//       Reset
-//     </button>
-//     {/* The && operator here makes it so that the following JSX is only added if the expression is truthy */}
-//     {this.state.gameOver && <p>{this.state.message}</p>}
-//     <h4>Scoreboard:</h4>
-//     <p>X: {X}</p>
-//     <p>O: {O} </p>
-//   </div>
-// );
-// }
-// }
 
 export default Board;
--- src/client/Components/Row.tsx
+++ src/client/Components/Row.tsx
@@ -22,16 +22,4 @@ function Row(props: RowProps) {
   return <div className="row">{boxes}</div>;
 }
 
-// const Row2 = (props: RowProps) => {
-//   const { content, row, handleBoxClick } = props;
-
-//   const boxes: JSX.Element[] = content.map((box: any, i: any) => {
-//     return (
-//       <Box text={box} row={row} column={i} handleBoxClick={handleBoxClick} />
-//     );
-//   });
-
-//   return <div className="row">{boxes}</div>;
-// };
-
 export default Row;
