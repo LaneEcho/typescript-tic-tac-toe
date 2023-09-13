43552e79173045fcc8d45a49254ccdc61edc40b9
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-09-12 17:16:10 -0700

    Typography

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Board from './Components/Board';
 
+import { Typography } from '@mui/material';
+
 function App() {
   return (
     <div>
-      <h1>Tic Tac Toe</h1>
+      <Typography variant="h1" color="primary">
+        Tic-Tac-Toe
+      </Typography>
       <Board />
     </div>
   );
