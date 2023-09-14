20880f68e63fe2a0e4c7b9e03d2bc5f4f9ec6903
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-09-14 15:42:26 -0700

    toggle dark mode

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import Board from './Components/Board';
 
-import { Typography } from '@mui/material';
+import {
+  Typography,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Button,
+} from '@mui/material';
 
 function App() {
+  // State to manage the current theme mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Create a light theme
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
+
+  // Create a dark theme
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  // Toggle the theme mode
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div>
-      <Typography variant="h1" color="primary">
-        Tic-Tac-Toe
-      </Typography>
-      <Board />
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <CssBaseline /> {/* Normalize CSS */}
+      <div>
+        <Typography variant="h1" color="primary">
+          Tic-Tac-Toe
+        </Typography>
+        <Button onClick={toggleTheme} variant="outlined">
+          Toggle {darkMode ? 'Light' : 'Dark'} Mode
+        </Button>
+        <Board />
+      </div>
+    </ThemeProvider>
   );
 }
 
